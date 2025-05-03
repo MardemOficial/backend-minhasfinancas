5530356684418b13ff4c1dfd1e038cd2dc02356e
Mardem Paz <mardenexecutive@gmail.com>
2025-05-03 18:20:56 -0300

    feat: Arquivos iniciais do projeto.

---
+++ server.js
@@ -0,0 +1,8 @@
+import app from "./src/app.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.info("Server is running on port", process.env.SERVER_PORT);
+})
