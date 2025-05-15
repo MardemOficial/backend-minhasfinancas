17da5ac502e1ebdee9502427d0987a3abc576c35
Mardem Paz <mardenexecutive@gmail.com>
2025-05-14 22:11:29 -0300

    feat: Foi feito a importação de propriedades para retornas os objetos preenchidos no json.

--- src/model/Transacao.js
+++ src/model/Transacao.js
@@ -1,18 +1,21 @@
 import { DataTypes } from "sequelize"; 
 import sequelize from "../config/db.js";
+import Fornecedor from "./Fornecedor.js";
+import Subcategoria from "./Subcategoria.js";
+import Conta from "./Conta.js";
 
 const Transacao = sequelize.define('transacao', {
     id : {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, field:'id_transacao'},
     data_transacao : {type: DataTypes.DATE, allowNull: false},
     valor : {type: DataTypes.FLOAT, allowNull: false},
-    id_subcategoria : {type: DataTypes.INTEGER, allowNull: false},
-    id_fornecedor : {type: DataTypes.INTEGER, allowNull: false},
-    id_conta : {type: DataTypes.INTEGER, allowNull: false}
 }, {
     tableName: 'transacoes',
     timestamps: false
-}
+});
 
-);
+Transacao.hasOne(Fornecedor, {as: "fornecedor", foreignKey: 'id'});
+Transacao.hasOne(Subcategoria, {as: "subcategoria", foreignKey: 'id'});
+Transacao.hasOne(Conta, {as: "conta", foreignKey: 'id'});
+
 
 export default Transacao;   
