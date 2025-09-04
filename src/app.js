f442d3b82c53a6b4642d45fc01cfd9254c51ed5d
Mardem Paz <mardenexecutive@gmail.com>
2025-09-04 20:42:32 -0300

    feat: Add error handling middleware.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
 import routes from "./routes/index.js";
+import cors from "cors";
+import middlewareException from "./middleware/middlewareException.js";
 
 const app = express();
+app.use(cors());
+
 routes(app);
+
+app.use(middlewareException);
 
 export default app;
